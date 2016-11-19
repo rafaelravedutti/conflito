034b98c19d9d7c0d80197ac0854dbd22c67a824e
Rafael Ravedutti <rafaelraveduttilm@hotmail.com>
2016-11-19 12:16:04 -0200

    Write check_seriability function
    Signed-off-by: Rafael Ravedutti <rafaelraveduttilm@hotmail.com>

--- conflito.c
+++ conflito.c
@@ -21,26 +21,85 @@ struct ui_list {
 
 struct graph {
   struct vertex *graph_vertices;
+  unsigned int graph_nvertices;
 };
 
 struct vertex {
   unsigned int vertex_id;
+  unsigned int vertex_number;
   struct edge *vertex_edges;
   struct vertex *vertex_next;
 };
 
 struct edge {
-  unsigned int edge_source;
-  unsigned int edge_dest;
+  struct vertex *edge_dest;
   struct edge *edge_next;
 };
 
+/* Seriability test functions */
+int check_seriability(struct graph *graph);
+int _check_seriability(struct vertex *vertex, char *visited);
+
+/* Graph functions */
+void init_graph(struct graph *graph);
+void add_vertex(struct graph *graph, unsigned int vertex_id);
+void add_edge(struct graph *graph, unsigned int source, unsigned int dest);
+struct vertex *find_vertex_by_id(struct graph *graph, unsigned int id);
+void clear_graph(struct graph *graph);
+
+/* List functions */
+void init_list(struct ui_list *list);
+int insert(struct ui_list *list, unsigned int element);
+void delete(struct ui_list *list, unsigned int element);
+int find(struct ui_list *list, unsigned int element);
+int empty(struct ui_list *list);
+
+
 int check_seriability(struct graph *graph) {
-  return 0;
+  struct vertex *vertex;
+  char *visited;
+  unsigned int i;
+
+  visited = (char *) malloc(sizeof(char) * graph->graph_nvertices);
+
+  if(visited != NULL) {
+    for(i = 0; i < graph->graph_nvertices; ++i) {
+      visited[i] = 0;
+    }
+
+    for(vertex = graph->graph_vertices; vertex != NULL; vertex = vertex->vertex_next) {
+      if(visited[vertex->vertex_number] == 0) {
+        visited[vertex->vertex_number] = 1;
+        if(_check_seriability(vertex, visited) == 0) {
+          free(visited);
+          return 0;
+        }
+      }
+    }
+  }
+
+  free(visited);
+  return 1;
+}
+
+int _check_seriability(struct vertex *vertex, char *visited) {
+  struct edge *edge;
+
+  for(edge = vertex->vertex_edges; edge != NULL; edge = edge->edge_next) {
+    if(visited[edge->edge_dest->vertex_number] == 1) {
+      return 0;
+    }
+
+    visited[edge->edge_dest->vertex_number] = 1;
+    _check_seriability(edge->edge_dest, visited);
+  }
+
+  return 1;
 }
 
 void init_graph(struct graph *graph) {
   graph->graph_vertices = NULL;
+  graph->graph_nvertices = 0;
 }
 
 void add_vertex(struct graph *graph, unsigned int vertex_id) {
@@ -51,9 +110,11 @@ void add_vertex(struct graph *graph, unsigned int vertex_id) {
 
     if(vertex != NULL) {
       vertex->vertex_id = vertex_id;
+      vertex->vertex_number = graph->graph_nvertices;
       vertex->vertex_edges = NULL;
       vertex->vertex_next = graph->graph_vertices;
       graph->graph_vertices = vertex;
+      graph->graph_nvertices++;
     }
   }
 }
@@ -68,14 +129,25 @@ void add_edge(struct graph *graph, unsigned int source, unsigned int dest) {
         edge = (struct edge *) malloc(sizeof(struct edge));
 
         if(edge != NULL) {
-          edge->edge_source = source;
-          edge->edge_dest = dest;
+          edge->edge_dest = find_vertex_by_id(graph, dest);
           edge->edge_next = vertex->vertex_edges;
           vertex->vertex_edges = edge;
         }
       }
     }
   }
+}
+
+struct vertex *find_vertex_by_id(struct graph *graph, unsigned int id) {
+  struct vertex *vertex;
+
+  for(vertex = graph->graph_vertices; vertex != NULL; vertex = vertex->vertex_next) {
+    if(vertex->vertex_id == id) {
+      return vertex;
+    }
+  }
+
+  return NULL;
 }
 
 void clear_graph(struct graph *graph) {
@@ -95,6 +167,8 @@ void clear_graph(struct graph *graph) {
 
       free(vertex);
     }
+
+    graph->graph_nvertices = 0;
   }
 }
 
@@ -200,5 +274,6 @@ int main(int argc, const char *argv[]) {
     }
   }
 
+  clear_graph(&transaction_graph);
   return 0;
 }
